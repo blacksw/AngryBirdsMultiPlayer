b3791e50ed2be13627f6aa44547ff4ca9de64313
blacksw <sweetiki57@gmail.com>
2019-03-02 13:08:09 +0000

    pp

--- messageForm/index.js
+++ messageForm/index.js
@@ -1,4 +1,17 @@
+
+var rect = 0; 
 function readInput(){
-	var input = document.getElementById('messageArea').value;
-	var message = document.createElement("div");
+	var input = document.getElementById('messageArea');
+	var message = document.createElement("p");
+
+	document.body.appendChild(message);
+	
+	message.setAttribute("class","message");
+	message.innerHTML = input.value;
+
+	message.style.top = rect+"px";
+	var p = $(".message");
+	var position = p.position();
+	rect += position.top+35;
+	console.log(position.top);
 }
