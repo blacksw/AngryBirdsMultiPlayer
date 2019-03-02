be207d7447bf54b86f889552d3e4e8a37563574c
blacksw <sweetiki57@gmail.com>
2019-03-02 16:44:14 +0000

    responsive design

--- messageForm/index.js
+++ messageForm/index.js
@@ -3,15 +3,36 @@ var rect = 0;
 function readInput(){
 	var input = document.getElementById('messageArea');
 	var message = document.createElement("p");
-
+	var string = input.value;
+	if(string != ""){
 	document.body.children[1].appendChild(message);
 	
 	message.setAttribute("class","message");
-	message.innerHTML = input.value;
+	message.innerHTML = string;
 
 	message.style.top = rect+"px";
 	var p = $(".message");
 	var position = p.position();
-	rect += position.top+35;
+	rect += position.top+45;
 	console.log(position.top);
+	}
+}
+function readInputkey(){
+	
+	var input = document.getElementById('messageArea');
+	var message = document.createElement("p");
+	var string = input.value;
+	if(string != "" && event.keyCode == 13){
+	document.body.children[1].appendChild(message);
+	
+	message.setAttribute("class","message");
+	message.innerHTML = string;
+
+	message.style.top = rect+"px";
+	var p = $(".message");
+	var position = p.position();
+	rect += position.top+45;
+	console.log(position.top);
+		}
+	
 }
